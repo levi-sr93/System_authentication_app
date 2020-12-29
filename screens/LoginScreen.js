6c0b4a68168758de98e4331917f78fe4d06b3844
levi-sr93 <levi-sr@outlook.com>
2020-12-28 22:53:23 -0300

    Login Screen| Formik|

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,124 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native'
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  KeyboardAvoidingView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Formik } from "formik";
+import { block } from "react-native-reanimated";
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
-  )
-}
+    <KeyboardAvoidingView>
+      <ScrollView>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          onSubmit={(formValues) => {
+            console.log(formValues);
+          }}
+        >
+          {(props) => (
+            <View style={styles.container}>
+              <View style={styles.logo}>
+                <Image
+                  style={styles.image}
+                  source={require("../assets/images/authSystem.jpg")}
+                />
+              </View>
+              <View>
+                <TextInput
+                  style={styles.inputBox}
+                  placeholder="Email"
+                  placeholderTextColor="#fff"
+                  keyboardType="email-address"
+                />
+                <TextInput
+                  style={styles.inputBox}
+                  placeholder="Password"
+                  placeholderTextColor="#fff"
+                  secureTextEntry={true}
+                />
+                <TouchableOpacity style={styles.button}>
+                  <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+
+                <View style={styles.registerContainer}>
+                  <Text style={styles.registerText}>
+                    Don't have an account?
+                  </Text>
+                  <TouchableOpacity style={styles.registerButton}>
+                    <Text>Register</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          )}
+        </Formik>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
 
 const styles = StyleSheet.create({
-
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  logo: {
+    alignItems: "center",
+    marginBottom: 40,
+    marginTop: 50,
+  },
+  image: {
+    width: 100,
+    height: 80,
+    borderRadius: 15,
+  },
+  inputBox: {
+    width: 300,
+    backgroundColor: "#b6bfc4",
+    borderRadius: 25,
+    padding: 16,
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  button: {
+    width: 300,
+    backgroundColor: "#728289",
+    borderRadius: 25,
+    marginVertical: 10,
+    paddingVertical: 13,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: "#fff",
+    textAlign: "center",
+  },
+  registerContainer: {
+    alignItems: "flex-end",
+    justifyContent: "center",
+    paddingVertical: 16,
+    flexDirection: "row",
+  },
+  registerText: {
+    color: "#738289",
+    fontSize: 16,
+  },
+  registerButton: {
+    color: "#738289",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
 export default LoginScreen;
