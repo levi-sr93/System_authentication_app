a24c237f54b8ac14e0b4dad3ae9a7570c68d5114
levi-sr93 <levi-sr@outlook.com>
2020-12-28 21:10:13 -0300

    Setting up Initial sctructure

---
+++ screens/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native'
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+export default LoginScreen;
