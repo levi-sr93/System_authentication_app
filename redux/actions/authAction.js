d686b91209f58374f26e8aad91384e44bd30133e
levi-sr93 <levi-sr@outlook.com>
2021-01-01 19:53:18 -0300

    Creating ACTIONS| registerUser | loginUser

--- redux/actions/authAction.js
+++ redux/actions/authAction.js
@@ -4,7 +4,7 @@ export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
 export const REGISTER_USER_FAILURE = "REGISTER_USER_FAILURE";
 
 export const registerUser = (authData) => {
-  //logic to register user
+  //logic to REGISTER user
   const { fullName, email, password } = data;
 
   //dispatch function to register Function
@@ -22,7 +22,7 @@ export const loginUser = (authData) => {
   //logic to register user
   const { email, password } = data;
 
-  //dispatch function to register Function
+  //dispatch function to LOGIN Function
   //the benefits of this approach is because here we can add logic to make a post to create user
 
   return async (dispatch) => {
