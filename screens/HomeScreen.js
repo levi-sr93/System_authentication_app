f345bdb1804a53ae4c0169b633d2920471bcb26f
levi-sr93 <levi-sr@outlook.com>
2021-01-02 11:51:15 -0300

    Fetching Token from device localStorage| AsyncStorage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, StyleSheet, Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
+  const loadProfile = async () => {
+    const token = await AsyncStorage.getItem("token");
+    if (!token) {
+      props.navigation.navigate("Login");
+    }
+    console.log(token);
+  };
+
+  useEffect(() => {
+    loadProfile();
+  });
+
   return (
     <View>
       <Text>Login Screen</Text>
